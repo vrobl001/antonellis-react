1c712338fdfea3c53510d2de46a3700909377fd6
Vincent Robles <sylkisgsx@gmail.com>
2020-08-20 14:26:53 -0400

    initial user service

---
+++ src/utils/userService.js
@@ -0,0 +1,51 @@
+import tokenService from './tokenService';
+
+const BASE_URL = '/api/users/';
+
+function login(creds) {
+  return fetch(BASE_URL + 'login', {
+    method: 'POST',
+    headers: new Headers({ 'Content-type': 'Application/json' }),
+    body: JSON.stringify(creds),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Incorrect log in info!');
+      }
+    })
+    .then(({ token }) => tokenService.setToken(token));
+}
+
+function logout() {
+  tokenService.removeToken();
+}
+
+function getUser() {
+  return tokenService.getUserFromToken();
+}
+
+function signup(user) {
+  return fetch(BASE_URL + 'signup', {
+    method: 'POST',
+    headers: new Headers({ 'Content-type': 'Application/json' }),
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log(response);
+        throw new Error(response);
+      }
+    })
+    .then(({ token }) => tokenService.setToken(token));
+}
+
+export default {
+  signup,
+  getUser,
+  logout,
+  login,
+};
