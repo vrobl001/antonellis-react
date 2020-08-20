1724e3b4c43ee16a5e534ded59176f1343b89db4
Vincent Robles <sylkisgsx@gmail.com>
2020-08-20 14:25:19 -0400

    initial catering page

---
+++ src/pages/Catering/Catering.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Catering.module.css';
+
+export default function Catering(props) {
+  return (
+    <div>
+      <h1>Catering Page</h1>
+    </div>
+  );
+}
