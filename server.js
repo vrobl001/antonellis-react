a36c9a2174e427824f57af6cdd92690bf706f87f
Vincent Robles <sylkisgsx@gmail.com>
2020-08-22 13:10:37 -0400

    mounted user and profile route

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ app.use(serveFavicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(require('./config/auth'));
-app.use('/api/users', require('./routes/api/users'));
+app.use('/api/user', require('./routes/api/user'));
 app.use('/api/food', require('./routes/api/food'));
 app.use('/api/blog', require('./routes/api/blog'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
