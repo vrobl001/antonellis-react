5df7073b14c59c61343867b613ffd19ce970899b
Vincent Robles <sylkisgsx@gmail.com>
2020-08-22 13:09:00 -0400

    initial coupons page

---
+++ src/pages/Coupons/Coupons.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Coupons() {
+  return (
+    <div className='pageContainer'>
+      <h1>Coupons</h1>
+    </div>
+  );
+}
